57b3e757754a7a76b43cf4d14d8bbca38b1ce1dc
JEONGSUJONG <jeon95ujong@gmail.com>
2024-02-21 22:43:06 +0900

    Feat : Product Upload

--- BE/src/middleware/auth.js
+++ BE/src/middleware/auth.js
@@ -4,7 +4,7 @@ const User = require("../models/user-schema");
 let auth = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (token === null) return res.sendStatus(401);
+  if (!token) return res.sendStatus(401);
 
   try {
     const decode = jwt.verify(token, process.env.JWT_SECRET);
